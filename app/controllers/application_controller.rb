eb2bfd8e86f6704e67578ba7b6a617816a66e39a
imaccho <june6_0302@yahoo.co.jp>
2022-10-05 19:47:17 +0900

    usersテーブルにデータ保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  # binding.pry
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birth_date])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
