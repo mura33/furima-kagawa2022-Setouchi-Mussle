e0bf5b500049fc89af6be587355847f7da49c7db
imaccho <june6_0302@yahoo.co.jp>
2022-10-06 17:58:35 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true           
-  validates :first_name, presence: true          
-  validates :last_name_kana, presence: true       
-  validates :first_name, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+   
+  with_options presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: 'Input full-width katakana characters' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
   validates :birth_date, presence: true
   
 end
