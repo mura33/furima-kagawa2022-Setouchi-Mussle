916da717fcbb943356cb0d7957536803f0b9234e
mura33 <ozsn81.ui@gmail.com>
2022-10-07 17:22:56 +0900

    価格の制限

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,5 +14,9 @@ class Item < ApplicationRecord
   validates :image, :name, :info, presence: true
 
   validates :category_id, :prefecture_id, :sales_status_id, :scheduled_delivery_id, :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank" }
-  
+
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to:300, less_than_or_equaal_to: 9_999_999},
+                    presence: { message: "can't be blank"}
+  end
 end
