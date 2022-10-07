a7e6cc40aca1aeb6de6cb6904d65e5db6e75b289
mura33 <ozsn81.ui@gmail.com>
2022-10-07 14:02:17 +0900

    createアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,16 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :image).merge(user_id: current_user.id)
   end
 
 end
