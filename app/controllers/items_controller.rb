2ef6b7cd04ab6e4818162900a8257daeade19fa2
mura33 <ozsn81.ui@gmail.com>
2022-10-08 15:36:09 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
 
   def new
-    unless user_signed_in? 
-      redirect_to new_user_session_path
-    end
     @item = Item.new
   end
 
