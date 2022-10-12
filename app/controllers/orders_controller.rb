2758b18c8e68364735bd51d58abac53f2589b589
H-Kitaura <110322542+H-Kitaura@users.noreply.github.com>
2022-10-12 20:55:06 +0900

    商品登録画面表示実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   def index
+    @order = Item.find(params[:item_id])
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :orders, only: [:index, :create]
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+   resources :orders, only: [:index, :create]
+  end
 end
