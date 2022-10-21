ebf84331ee49652f134225a3dc8c146cb8e8937f
satoooo1122 <shunsuke04140414@icloud.com>
2022-10-21 17:21:58 +0900

    リポジトリの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.1'
 set :application, 'furima-kagawa2022-Setouchi-Mussle'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:satoooo1122/furima-kagawa2022-Setouchi-Mussle.git'
+set :repo_url,  'git@github.com:mura33/furima-kagawa2022-Setouchi-Mussle.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
