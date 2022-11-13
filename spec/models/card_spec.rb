b950da811667a37d5b7b0a9e1f124db11484d720
mura33 <ozsn81.ui@gmail.com>
2022-11-13 14:01:54 +0900

    テストコード記述

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @card = FactoryBot.create(:card)
+  end
+
+  describe 'カード登録の記入' do
+    context '内容に問題がない場合' do
+      it 'すべての値が正常に登録されていれば保存できる' do
+        expect(@card).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'card_tokenが空では登録できない' do
+        @card.card_token = ''
+        @card.valid?
+        expect(@card.errors.full_messages).to include("Card tokenを入力してください")
+      end
+
+      it 'user_idが空では登録できない' do
+        @card.user_id = ''
+        @card.valid?
+        expect(@card.errors.full_messages).to include("Userを入力してください")
+      end
+    end
+  end
 end
